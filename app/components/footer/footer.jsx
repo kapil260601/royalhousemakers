5da311dae11473e56adf62afd2f59bec8c12e5e7
kapil260601 <88288424+kapil260601@users.noreply.github.com>
2024-04-26 16:24:35 +0530

    Final Chnages

--- app/components/footer/footer.jsx
+++ app/components/footer/footer.jsx
@@ -13,15 +13,15 @@ function Footer() {
     return (
         <>
            <footer className="footer-section px-3">
-            <div className="container">
+            <div className="pt-5">
                 <div className="footer-cta pt-3 pb-3">
                     <div className="row">
                         <div className="col-md-6 col-lg-3 mt-2">
-                            <div className="single-cta">
+                            <div className="single-cta d-flex justify-content-start">
                             <HomeSharpIcon style={{backgroundColor: "#F95911",color: "#FFFFFF",borderRadius:" 30%",marginRight: "10px", width:"40px", height:"37px", padding:"6px",borderRadius:"50%", marginBottom:"188px"}} />
                                 <div className="cta-text ms-5">
                                     <h4 className="text-start">Find us</h4>
-                                    <a className="Cursor" href="#"><p className="text-start" style={{color:"#AB8282"}}>6 A Shivampuri colony,<br /> Bholaram Ustadmarg,<br /> Indore, 452001</p> </a>
+                                    <a className="Cursor" href="#"><p className="text-start" style={{color:"#AB8282", fontSize:"18px"}}>6 A Shivampuri colony,<br /> Bholaram Ustadmarg,<br /> Indore, 452001</p> </a>
                                    
 
                                 </div>
@@ -32,22 +32,22 @@ function Footer() {
                             <CallSharpIcon style={{backgroundColor: "#F95911",color: "#FFFFFF",borderRadius:" 30%",marginRight: "10px", width:"40px", height:"37px", padding:"6px",borderRadius:"50%", marginBottom:"46px"}} />
                                  <div className="cta-text ms-5">
                                     <h4 className="text-start">Call us</h4>
-                                    <a className="Cursor" href="#"><span style={{color:"#AB8282"}} className="text-start">9009700155</span></a>
+                                    <a className="Cursor" href="#"><span style={{color:"#AB8282",fontSize:"18px"}} className="text-start">9009700155</span></a>
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-6 col-lg-3 mt-2">
                             <div className="single-cta">
-                            <MailOutlineSharpIcon style={{backgroundColor: "#F95911",color: "#FFFFFF",borderRadius:" 30%",marginRight: "10px", width:"40px", height:"37px", padding:"6px",borderRadius:"50%",marginBottom:"46px"}} />
+                            <MailOutlineSharpIcon style={{backgroundColor: "#F95911",color: "#FFFFFF",borderRadius:" 30%",marginRight: "53px", width:"40px", height:"37px", padding:"6px",borderRadius:"50%",marginBottom:"46px"}} />
                                 <div className="cta-text ">
                                     <h4 className="text-start">Mail us</h4>
-                                    <a className="Cursor" href="#"><span style={{color:"#AB8282"}} className="text-start">royalhousemakers@gmail.com</span></a>
+                                    <a className="Cursor" href="#"><span style={{color:"#AB8282", fontSize:"18px"}} className="text-start">royalhousemakers@gmail.com</span></a>
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-6 col-lg-3 mt-2">
                             <div className="footer-social-icon single-cta" style={{display:"flex"}}>
-                            <AddCircleOutlineSharpIcon style={{backgroundColor: "#F95911",color: "#FFFFFF",borderRadius:" 30%",marginRight: "10px", width:"40px", height:"37px", padding:"6px",borderRadius:"50%"}} />
+                            <AddCircleOutlineSharpIcon style={{backgroundColor: "#F95911",color: "#FFFFFF",borderRadius:" 30%",marginRight: "0px", width:"40px", height:"37px", padding:"6px",borderRadius:"50%"}} />
                                 <span>Follow: </span>
                                 <a className="Icons" href="#"><FacebookSharpIcon style={{backgroundColor: "#F95911",color: "#FFFFFF",borderRadius:" 30%",marginRight: "10px", width:"40px", height:"37px", padding:"6px",borderRadius:"50%"}} /></a>
                                 <a className="Icons" href="#">                    <CallSharpIcon style={{backgroundColor: "#F95911",color: "#FFFFFF",borderRadius:" 30%",marginRight: "10px", width:"40px", height:"37px", padding:"6px",borderRadius:"50%"}} /></a>
@@ -59,7 +59,7 @@ function Footer() {
                 </div>
                 <div className="footer-content">
                     <div className="row">
-                        <div className="col-md-6 col-lg-4">
+                        <div className="col-md-6 col-lg-4 col-sm-12 d-flex justify-content-center">
                             <div className="footer-widget">
                                 <div className="footer-logo">
                                     {/* <Link to="/"> */}
@@ -73,17 +73,19 @@ function Footer() {
                                 <div className="footer-widget-heading mt-4">
                                     <h3 className="text-center">Useful Links</h3>
                                 </div>
-                                <ul className="color_footer">
+                                
+                                <ul className="color_footer" style={{marginLeft:"65px"}}>
                                 <li style={{color:"#AB8282"}}><a href="#">Home</a></li>
-                                    <li><a href="#">About</a></li>
+                                    <li style={{color:"#AB8282",}}><a href="#">About</a></li>
                                     <li style={{color:"#AB8282"}}> <a href="#">Services</a></li>
-                                    <li><a href="#">Portfolio</a></li>
+                                    <li style={{color:"#AB8282"}}><a href="#">Portfolio</a></li>
                                     <li style={{color:"#AB8282"}}>  <a href="#">Contact</a></li>
-                                    <li><a href="#">Expert Team</a></li>
+                                    <li style={{color:"#AB8282"}}><a href="#">Expert Team</a></li>
                                 </ul>
+                               
                             </div>
                         </div>
-                        <div className="col-md-6 col-lg-4">
+                        <div className= "col-md-6 col-lg-4">
                             <div className="footer-widget">
                                 <div className="footer-widget-heading">
                                     <h3 className="text-center">Subscribe</h3>
@@ -93,7 +95,7 @@ function Footer() {
                                 </div>
                                 <div className="subscribe-form">
                                     <form action="#">
-                                        <input type="text" placeholder="Email Address" />
+                                        <input style={{color:"white"}} type="text" placeholder="Email Address" />
                                         <button><i className="fab fa-email-plane"></i>Subscribe</button>
                                     </form>
                                 </div>
@@ -107,7 +109,7 @@ function Footer() {
                     <div className="row" style={{ padding: '10px 0px' }}>
                         <div className="col-12 text-center">
                             <div className="copyright-text">
-                                <p>Copyright &copy; 2024, All Right Reserved <a href="#">Royal House Makers</a></p>
+                                <p style={{fontSize:"18px"}}>Copyright &copy; 2024, All Right Reserved <a href="#">Royal House Makers</a></p>
                             </div>
                         </div>
                     </div>
